81052a6751d2d9fc77ef30859cbb0c088955f249
Niki Chittela <n.chittela@gmail.com>
2020-12-14 12:35:21 -0500

    added register-events variables to section-vars

--- inc/section_vars.php
+++ inc/section_vars.php
@@ -5,5 +5,11 @@
   $home_top_desc = 'home_top_desc';
 
   $events_section = 'events_section';
+  $event_title = 'event_title';
   $events_poster_img = 'events_poster_img';
+  $date_desc = 'date_description';
+  $upcom_meet_desc = 'upcoming_meeting_description';
+  $location_desc = 'location_description';
+  $event_desc = 'description_section';
+  $rel_link_desc = 'relevant_link_description';
 ?>
