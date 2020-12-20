09813d3d88c81898657f3c27b6905bc99d960ffa
roushc-ux <55108021+roushc-ux@users.noreply.github.com>
2020-12-19 23:36:58 -0500

    Hard coded shop page

--- inc/add_styles.php
+++ inc/add_styles.php
@@ -5,6 +5,7 @@ function add_custom_styles() {
     wp_register_style("header", get_template_directory_uri() . "/css/header.css", '', '1.0.0');
     wp_register_style("footer", get_template_directory_uri() . "/css/footer.css", '', '1.0.0');
     wp_enqueue_style('footer');
+    wp_register_style("shop", get_template_directory_uri() . "/css/shop.css", '', '1.0.0');
     wp_enqueue_script("faq", get_template_directory_uri() . "/js/faq.js", '', '1.0.0');
     wp_enqueue_script("header", get_template_directory_uri() . "/js/header.js", '', '1.0.0');
 }
--- page-shop.php
+++ page-shop.php
@@ -1,12 +1,200 @@
 <?php 
   get_header(); 
+  wp_enqueue_style('shop');
 ?>
-
-<h1>Shop</h1>
-
-<form class="message-form" method="post">
-  <input id="message-title" type="text">
-  <button  type="submit">Save!</button>
-</form>
-
+  <section id="description-shop-container">
+    <span class="bracket-shop">[</span>
+    <span id="description-header">
+      MICHELLE'S SHOP
+    </span>
+    <span class="bracket-shop">]</span>
+  </section>
+  <div class="shop-wrapper">
+    <!-- Categories -->
+    <div class="shop-category-container">
+      <div class="shop-category-header">
+        Categories
+      </div>
+      <div class="categories">
+        <div class="category">
+          All
+        </div>
+        <div class="category">
+          Kids
+        </div>
+        <div class="category">
+          Mystery
+        </div>
+        <div class="category">
+          Nonfiction
+        </div>
+        <div class="category">
+          Fiction
+        </div>
+        <div class="category selected">
+          Romance
+        </div>
+        <div class="category">
+          History
+        </div>
+        <div class="category">
+          Young Adult
+        </div>
+        <div class="category">
+          Poetry
+        </div>
+        <div class="category">
+          Journals
+        </div>
+        <div class="category">
+          Christian Fiction
+        </div>
+        <div class="category">
+          Fantasy
+        </div>
+        <div class="category">
+          Memoir
+        </div>
+        <div class="category">
+          Current Events
+        </div>
+        <div class="category">
+          Travel
+        </div>
+        <div class="category">
+          Nature
+        </div>
+        <div class="category">
+          Cooking
+        </div>
+        <div class="category">
+          Spiritual
+        </div>
+        <div class="category">
+          Essays
+        </div>
+        <div class="category">
+          Self-help
+        </div>
+      </div>
+      <div class="shop-category-header">
+        Condition
+      </div>
+      <div class="categories">
+        <div class="category">
+          All
+        </div>
+        <div class="category selected">
+          New
+        </div>
+        <div class="category">
+          Used
+        </div>
+      </div>
+    </div>
+    <!-- Shop results and category picker -->
+    <div class="shop-search-container">
+      <!-- Filters -->
+      <div class="shop-filters-container">
+          <div class="filter-box gray">
+            <div class="filter-text">
+              Show 60
+            </div>
+            <img class="filter-arrow" src=<?php echo get_template_directory_uri() . "/photos/arrow.png" ?> height="20px" alt="Arrow">
+          </div>
+          <div class="filter-box pink">
+            <div class="filter-text">
+              Popularity
+            </div>
+            <img class="filter-arrow" src=<?php echo get_template_directory_uri() . "/photos/arrow.png" ?> height="20px" alt="Arrow">
+          </div>
+      </div>
+       <!-- Search Results -->
+       <div class="search-grid">
+        <div class="book-container">
+          <img class="book-img" src=<?php echo get_template_directory_uri() . "/photos/book-img-example.png" ?> alt="Book Image">
+          <div class="book-title">
+            Educated
+          </div>
+          <div class="book-price">
+            $15.99
+          </div>
+        </div>
+        <div class="book-container">
+          <img class="book-img" src=<?php echo get_template_directory_uri() . "/photos/book-img-example.png" ?> alt="Book Image">
+          <div class="book-title">
+            Educated
+          </div>
+          <div class="book-price">
+            $15.99
+          </div>
+        </div>
+        <div class="book-container">
+          <img class="book-img" src=<?php echo get_template_directory_uri() . "/photos/book-img-example.png" ?> alt="Book Image">
+          <div class="book-title">
+            Educated
+          </div>
+          <div class="book-price">
+            $15.99
+          </div>
+        </div>
+        <div class="book-container">
+          <img class="book-img" src=<?php echo get_template_directory_uri() . "/photos/book-img-example.png" ?> alt="Book Image">
+          <div class="book-title">
+            Educated
+          </div>
+          <div class="book-price">
+            $15.99
+          </div>
+        </div>
+        <div class="book-container">
+          <img class="book-img" src=<?php echo get_template_directory_uri() . "/photos/book-img-example.png" ?> alt="Book Image">
+          <div class="book-title">
+            Educated
+          </div>
+          <div class="book-price">
+            $15.99
+          </div>
+        </div>
+        <div class="book-container">
+          <img class="book-img" src=<?php echo get_template_directory_uri() . "/photos/book-img-example.png" ?> alt="Book Image">
+          <div class="book-title">
+            Educated
+          </div>
+          <div class="book-price">
+            $15.99
+          </div>
+        </div>
+        <div class="book-container">
+          <img class="book-img" src=<?php echo get_template_directory_uri() . "/photos/book-img-example.png" ?> alt="Book Image">
+          <div class="book-title">
+            Educated
+          </div>
+          <div class="book-price">
+            $15.99
+          </div>
+        </div>
+        <div class="book-container">
+          <img class="book-img" src=<?php echo get_template_directory_uri() . "/photos/book-img-example.png" ?> alt="Book Image">
+          <div class="book-title">
+            Educated
+          </div>
+          <div class="book-price">
+            $15.99
+          </div>
+        </div>
+        <div class="book-container">
+          <img class="book-img" src=<?php echo get_template_directory_uri() . "/photos/book-img-example.png" ?> alt="Book Image">
+          <div class="book-title">
+            Educated
+          </div>
+          <div class="book-price">
+            $15.99
+          </div>
+        </div>
+       </div>
+    </div>
+    
+    
+  </div>
 <?php get_footer(); ?>
