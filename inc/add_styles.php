eb7c38e9d92ab7f61e677a1903da2feb1be21fef
Niki Chittela <n.chittela@gmail.com>
2020-12-20 19:56:45 -0500

    added event.js
    Also added with wp_enque_scripts

--- inc/add_styles.php
+++ inc/add_styles.php
@@ -3,5 +3,6 @@ function add_custom_styles() {
     wp_register_style("faq", get_template_directory_uri() . "/css/faq.css", '', '1.0.0');
     wp_register_style("events", get_template_directory_uri() . "/css/events.css", '', '1.0.0');
     wp_enqueue_script("faq", get_template_directory_uri() . "/js/faq.js", '', '1.0.0');
+    wp_enqueue_script("events", get_template_directory_uri() . "/js/events.js", '', '1.0.0');
 }
 add_action('wp_enqueue_scripts', 'add_custom_styles');
