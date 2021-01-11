a5be2520e829e4a6908f4d4b057cde260b1eab76
roushc-ux <55108021+roushc-ux@users.noreply.github.com>
2021-01-11 18:51:06 -0500

    Added to the searchbar to make it responsive

--- js/header.js
+++ js/header.js
@@ -1,3 +1,4 @@
+// search bar in desktop
 window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("search-icon").addEventListener("click", showSearchBar);
     document.getElementById("cancel").addEventListener("click", hideSearchBar);
@@ -9,5 +10,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
     function hideSearchBar() {
         document.getElementById('search-bar-container').style.display = 'none';
         document.getElementById('top-header').style.display = 'flex';
+    }
+});
+// search bar mobile
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("mobile-search-icon").addEventListener("click", showMobileSearchBar);
+    document.getElementById("mobile-cancel").addEventListener("click", hideMobileSearchBar);
+
+    function showMobileSearchBar() {
+        document.getElementById('images').style.display = 'none';
+        document.getElementById('mobile-search-bar-container').style.display = 'flex';
+    }
+    function hideMobileSearchBar() {
+        document.getElementById('mobile-search-bar-container').style.display = 'none';
+        document.getElementById('images').style.display = 'flex';
     }
 });
