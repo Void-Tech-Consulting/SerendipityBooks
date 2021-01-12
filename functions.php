9dcd0053e81dd336b343422134343a184041bc63
annapple <annapple@umich.edu>
2021-01-11 20:00:29 -0500

    name change

--- functions.php
+++ functions.php
@@ -6,16 +6,11 @@
   require get_template_directory() . '/inc/register-header-settings.php';
   require get_template_directory() . '/inc/register-footer-settings.php';
   require get_template_directory() . '/inc/register-book-settings.php';
-
   require get_template_directory() . '/inc/register-faq-settings.php';
   require get_template_directory() . '/inc/register-shop-settings.php';
-  require get_template_directory() . '/inc/add_styles.php';
-
-  require get_template_directory() . '/inc/register-faq.php';
-  require get_template_directory() . '/inc/register-shop.php';
 
   require get_template_directory() . '/inc/register-event-settings.php';
 
   require get_template_directory() . '/inc/add_styles.php';
-
+  
   require get_template_directory() . '/inc/register-favorite-type.php';
