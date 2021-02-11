dce844d07a77f52870de2326a05360e556fb4578
ericlau00 <ericlau41802@gmail.com>
2021-02-11 15:15:50 -0500

    Display search result books

--- search.php
+++ search.php
@@ -2,21 +2,55 @@
 get_header();
 wp_enqueue_style('search');
 
+function display_book($product) {
+    $name = $product->get_name();
+    $price = (float) $product->get_price();
+    $ean = $product->get_attribute('ean');
+
+    $price = number_format($price, 2);
+
+    $imgsrc = "http://covers.openlibrary.org/b/isbn/$ean-L.jpg";
+
+    echo "
+      <div class='book'>
+        <div class='book-card'>
+          <img 
+            alt='$name Book Cover' 
+            src=$imgsrc>
+        </div>
+        <div class='book-desc'>
+          <div class='book-title'>$name</div>
+          <div class='book-price'>$$price</div>
+        </div>
+      </div>";
+}
+
 if (have_posts()) :
-?>
-    hello, world
-<?php
+    $query = get_search_query();
+    echo '<section id="search-results-container">';
+    echo "<div id='search-query'>Search Results For : “{$query}”</div>";
+    echo '<section id="search-results">';
+    while (have_posts()) {
+        the_post();
+
+        $id = get_the_ID();
+        $product = wc_get_product($id);
+
+        display_book($product);
+      }
+    echo '</section>
+    </section>';
 else :
 ?>
     <section id="search-container">
         <div id="search-content">
             <div id="search-text-main">
-                “<?php echo get_search_query() ?>” was not found in our store. 
+                “<?php echo get_search_query() ?>” was not found in our store.
                 <br>Check the extended inventory <span class="highlight">here.</span>
             </div>
             <img width="75%" height="auto" alt="Search not found 404 error" src=<?php echo get_template_directory_uri() . "/img/not_found_alt.png" ?>>
             <div id="search-text-sub">
-                If you still haven't found what you wanted, please contact us at enteremail@gmail.com. 
+                If you still haven't found what you wanted, please contact us at enteremail@gmail.com.
                 <br>We'd love to work with you and help us find each other on <span class="highlight">the same page.</span>
             </div>
         </div>
