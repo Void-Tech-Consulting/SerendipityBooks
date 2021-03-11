6b29d841af84bc655b909c0d365d14b2eeca3c44
Ann <annie.wang50@yahoo.com>
2021-03-10 20:28:13 -0500

    author, img, description

--- js/book.js
+++ js/book.js
@@ -4,7 +4,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     function incrementQuantity() {
         var max = parseInt(document.getElementById('book-quantity').max);
-        console.log(document.getElementById('book-quantity').value < max);
         if (document.getElementById('book-quantity').value < max) {
             document.getElementById('book-quantity').value++;
         }
@@ -16,23 +15,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
-var isbn = $('#bookcover').data('isbn');
 
-$.ajax({
-  dataType: 'json',
-  url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn,
-  success: handleResponse
-});
+(function($) {
+  $(document).ready(function(){
+    var isbn = $('#bookcover').data('isbn');
+    $.ajax({
+      dataType: 'json',
+      url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn,
+      success: handleResponse
+    });
 
-function handleResponse( response ) {
-  $.each( response.items, function( i, item ) {
-    
-    var title    = item.volumeInfo.title,
-        author   = item.volumeInfo.authors[0],        
-        thumb    = item.volumeInfo.imageLinks.thumbnail;
-    
-    $('#booktitle').text( title );
-    $('#bookauthor').text( author );
-    $('#thumbnail').attr('src', thumb);
-  });
-}
+    function handleResponse( response ) {
+      $.each( response.items, function( i, item ) {
+        var title    = item.volumeInfo.title,
+            author   = item.volumeInfo.authors[0],      
+            descrip  = item.volumeInfo.description;  
+            thumb    = item.volumeInfo.imageLinks.thumbnail;
+            
+        var alen = author.length, name;
+        for (var i = alen - 1; i > 0; i--) {
+          console.log(author[i]);
+          if (author[i] == ' ') {
+            name = i;
+            break;
+          }
+        }
+        $('#booktitle').text( title );
+        $('#bookauthorlast').text( author.substring(name, alen) + ', ');
+        $('#bookauthorfirst').text( author.substring(0, name) + ' - ');
+        $('#descrip').text(descrip);
+        $('#thumbnail').attr('src', thumb);
+      });
+    }
+  })
+}(jQuery));
